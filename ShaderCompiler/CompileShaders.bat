00f2ab1251a164ff5e97478c849b28b3f418e12c
Nick De Breuck <nick.de.breuck@outlook.com>
2019-02-17 14:25:23 +0100

    updated bat file so shader bins get moved to right folder

--- ShaderCompiler/CompileShaders.bat
+++ ShaderCompiler/CompileShaders.bat
@@ -1,3 +1,6 @@
 glslangValidator.exe -V ../Data/Shaders/src/VulkanTest.vert
 glslangValidator.exe -V ../Data/Shaders/src/VulkanTest.frag
+
+MOVE frag.spv ../data/shaders/bin
+MOVE vert.spv ../data/shaders/bin
 pause
