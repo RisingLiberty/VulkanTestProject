c3486c0904a2305b1950e9b71a3cbd35ab72ba46
Nick De Breuck <nick.de.breuck@outlook.com>
2019-02-26 21:12:08 +0100

    buffer 2d class added

---
+++ Source/Vulkan/Buffer2D.cpp
@@ -0,0 +1,32 @@
+#include "Buffer2D.h"
+
+#include "LogicalDevice.h"
+#include "PhysicalDevice.h"
+#include "CommandPool.h"
+#include "RenderPass.h"
+#include "SwapChain.h"
+
+#include "../Help/HelperMethods.h"
+
+Buffer2D::Buffer2D(LogicalDevice* pCpu, CommandPool* pCommandPool, RenderPass* pRenderPass, PhysicalDevice* pGpu, uint32_t width, uint32_t height, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags flags, VkFormat format, VkImageAspectFlagBits aspectFlagBits, VkImageLayout oldLayout, VkImageLayout newLayout, uint32_t mipLevels) :
+	m_pCpu(pCpu)
+{
+	CreateImage(width, height, mipLevels,
+		pRenderPass->GetSamplesCount(), format,
+		tiling, usage, flags,
+		m_Image, m_ImageMemory,
+		pCpu, pGpu);
+
+	m_ImageView = CreateImageView(m_Image, format, VK_IMAGE_ASPECT_COLOR_BIT, mipLevels, pCpu);
+
+	TransitionImageLayout(m_Image, format, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, mipLevels, pCommandPool, pCpu);
+
+}
+
+Buffer2D::~Buffer2D()
+{
+	vkDestroyImageView(m_pCpu->GetDevice(), m_ImageView, nullptr);
+	vkDestroyImage(m_pCpu->GetDevice(), m_Image, nullptr);
+	vkFreeMemory(m_pCpu->GetDevice(), m_ImageMemory, nullptr);
+
+}
---
+++ Source/Vulkan/Buffer2D.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifndef GLFW_INCLUDE_VULKAN
+#define GLFW_INCLUDE_VULKAN
+#endif
+#include <GLFW/glfw3.h>
+
+class LogicalDevice;
+class PhysicalDevice;
+class CommandPool;
+class RenderPass;
+class SwapChain;
+
+class Buffer2D
+{
+public:
+	Buffer2D(LogicalDevice* pCpu, CommandPool* pCommandPool, RenderPass* pRenderPass, PhysicalDevice* pGpu, uint32_t width, uint32_t height, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags flags, VkFormat format, VkImageAspectFlagBits aspectFlagBits, VkImageLayout oldLayout, VkImageLayout newLayout, uint32_t mipLevels = 1);
+	~Buffer2D();
+
+	const VkImage& GetImage() const { return m_Image; }
+	const VkDeviceMemory& GetImageMemory() const { return m_ImageMemory; }
+	const VkImageView& GetImageView() const { return m_ImageView; }
+
+private:
+
+	//Trifecta of resources
+	VkImage m_Image;
+	VkDeviceMemory m_ImageMemory;
+	VkImageView m_ImageView;
+
+	LogicalDevice* m_pCpu;
+};
