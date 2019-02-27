3d3ccf30b98270daaa803495eb9a22dae803aedb
Nick De Breuck <nick.de.breuck@outlook.com>
2019-02-27 21:13:11 +0100

    no literals in constructor anymore

--- Source/Vulkan/Buffer2D.cpp
+++ Source/Vulkan/Buffer2D.cpp
@@ -8,7 +8,10 @@
 
 #include "../Help/HelperMethods.h"
 
-Buffer2D::Buffer2D(LogicalDevice* pCpu, CommandPool* pCommandPool, RenderPass* pRenderPass, PhysicalDevice* pGpu, uint32_t width, uint32_t height, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags flags, VkFormat format, VkImageAspectFlagBits aspectFlagBits, VkImageLayout oldLayout, VkImageLayout newLayout, uint32_t mipLevels) :
+Buffer2D::Buffer2D(LogicalDevice* pCpu, CommandPool* pCommandPool, RenderPass* pRenderPass, PhysicalDevice* pGpu, 
+	uint32_t width, uint32_t height, VkImageTiling tiling, VkImageUsageFlags usage, 
+	VkMemoryPropertyFlags flags, VkFormat format, VkImageAspectFlagBits aspectFlagBits, 
+	VkImageLayout oldLayout, VkImageLayout newLayout, uint32_t mipLevels) :
 	m_pCpu(pCpu)
 {
 	CreateImage(width, height, mipLevels,
@@ -17,9 +20,9 @@ Buffer2D::Buffer2D(LogicalDevice* pCpu, CommandPool* pCommandPool, RenderPass* p
 		m_Image, m_ImageMemory,
 		pCpu, pGpu);
 
-	m_ImageView = CreateImageView(m_Image, format, VK_IMAGE_ASPECT_COLOR_BIT, mipLevels, pCpu);
+	m_ImageView = CreateImageView(m_Image, format, aspectFlagBits, mipLevels, pCpu);
 
-	TransitionImageLayout(m_Image, format, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, mipLevels, pCommandPool, pCpu);
+	TransitionImageLayout(m_Image, format, oldLayout, newLayout, mipLevels, pCommandPool, pCpu);
 
 }
 
